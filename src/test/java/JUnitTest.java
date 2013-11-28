89f4bc66c66e0640e920d723d1f93b8a5b0b2f09
Stefan Birkner <mail@stefan-birkner.de>
2013-11-28 10:18:00 +0100

    Add test has_invalid_annotated_method.

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,16 +1,31 @@
 import static org.junit.Assert.assertEquals;
 
-import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
-import org.junit.internal.MethodSorter;
 import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.TestClass;
+
 import foobar.Sub;
 
 public class JUnitTest {
     @Test(expected = InitializationError.class)
-    public void inaccessibleBaseClassIsCaughtAtValidation() throws InitializationError {
+	public void inaccessible_base_class_is_caught_at_validation()
+			throws InitializationError {
         new BlockJUnit4ClassRunner(Sub.class);
     }
+
+	@Test
+	public void has_invalid_annotated_method()
+			throws Exception {
+		TestClass testClass = new TestClass(Sub.class);
+		FrameworkMethod annotatedMethod = testClass.getAnnotatedMethods(
+				Test.class).get(0);
+		List<Throwable> errors = new ArrayList<Throwable>();
+		annotatedMethod.validatePublicVoidNoArg(false, errors);
+		assertEquals("Wrong numer of errors.", 1, errors.size());
+	}
 }
