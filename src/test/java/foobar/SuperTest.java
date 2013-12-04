527481752e7a9ae4c125cfc66056b8be168a2063
Stefan Birkner <mail@stefan-birkner.de>
2013-12-04 01:01:01 +0100

    Add is_declared_class_of_its_method.

--- src/test/java/foobar/SuperTest.java
+++ src/test/java/foobar/SuperTest.java
@@ -1,14 +1,24 @@
 package foobar;
 
-import static java.lang.reflect.Modifier.*;
-import static org.junit.Assert.*;
+import static java.lang.reflect.Modifier.isPublic;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.lang.reflect.Method;
 
 import org.junit.Test;
 
 public class SuperTest {
-  @Test
-  public void isNotPublic() { 
-    int modifiers = Super.class.getModifiers();
-    boolean isPublic = isPublic(modifiers);
-    assertFalse("Super is public (modifiers: " + modifiers + ").", isPublic);     }
+	@Test
+	public void is_declared_class_of_its_method() throws Exception {
+		Method method = Super.class.getMethod("methodOfSuper");
+		assertEquals("foobar.Super", method.getDeclaringClass().getName());
+	}
+
+	@Test
+	public void isNotPublic() {
+		int modifiers = Super.class.getModifiers();
+		boolean isPublic = isPublic(modifiers);
+		assertFalse("Super is public (modifiers: " + modifiers + ").", isPublic);
+	}
 }
