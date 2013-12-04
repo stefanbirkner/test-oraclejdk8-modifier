6663f1641244bff664c5c81338853f3c54da52cd
Stefan Birkner <mail@stefan-birkner.de>
2013-12-04 01:21:33 +0100

    Replace FrameworkMemberTest with MethodTest.

--- src/test/java/foobar/MethodTest.java
+++ src/test/java/foobar/MethodTest.java
@@ -5,14 +5,12 @@ import static org.junit.Assert.assertEquals;
 import java.lang.reflect.Method;
 
 import org.junit.Test;
-import org.junit.runners.model.FrameworkMethod;
 
-public class FrameworkMemberTest {
+public class MethodTest {
 	@Test
 	public void does_not_have_annotation_defined_by_super() throws Exception {
 		Method method = Sub.class.getMethod("methodOfSuper");
-		FrameworkMethod frameworkMethod = new FrameworkMethod(method);
 		assertEquals("Wrong number of annotations", 0,
-				frameworkMethod.getAnnotations().length);
+				method.getAnnotations().length);
 	}
 }
